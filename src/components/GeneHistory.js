988fe47f3f9ec79a2e5772c05e23e97d51b143cd
Kenneth Bruskiewicz <kenneth@starinformatics.com>
2019-08-21 11:51:29 -0700

    DONE: GeneTransformerGraph sensitive to layout, WIP: GeneTransformerGraph data and codebase cleanup

--- src/components/GeneHistory.js
+++ src/components/GeneHistory.js
@@ -1,38 +1,9 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useRef, useState, useLayoutEffect} from "react"
+import ReactDOM from 'react-dom';
 import {FEATURE_FLAG} from "../parameters/FeatureFlags";
 import Card from "react-bootstrap/Card";
 import drawGraph from "../elements/D3/Graph/graph";
-
-export default function GeneHistory({geneListIDs, transactionHistory}) {
-    console.log(transactionHistory);
-    return (
-        <div className={"col-sm-2"}>
-            {geneListIDs.length > 0 && FEATURE_FLAG.histories.showHistories ?
-                <Card>
-                    <Card.Header as={"h5"}>
-                        History
-                    </Card.Header>
-                    <ul>
-                        {
-                            JSON.stringify(transactionHistory)
-
-                            // .slice(0).reverse().map(geneListID =>
-                            //  <li key={geneListID}> {geneListID} </li> )
-                        }
-                    </ul>
-                    <GeneTransformerGraph nodes={ledgerTo("nodes")(transactionHistory)} links={ledgerTo("links")(transactionHistory)}/>
-                </Card>
-                : <React.Fragment/>}
-        </div>
-    )
-}
-
-const GeneTransformerGraph = (props) => {
-    useEffect(() => {
-        drawGraph(props);
-    }, [props.nodes, props.links]);
-    return <div className={"viz"}/>;
-};
+import {SizeMe} from "react-sizeme";
 
 // ugh why am i writing this way
 const ledgerTo = (type) => {
@@ -50,3 +21,106 @@ const ledgerTo = (type) => {
 
     }
 }
+
+export default class GeneHistory extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"col-sm-2"}>
+                {this.props.geneListIDs.length > 0 && FEATURE_FLAG.histories.showHistories ?
+                    <Card>
+                        <Card.Header as={"h5"}>
+                            History
+                        </Card.Header>
+                        <ul>
+                            {
+                                JSON.stringify(this.props.transactionHistory)
+
+                                // .slice(0).reverse().map(geneListID =>
+                                //  <li key={geneListID}> {geneListID} </li> )
+                            }
+                        </ul>
+                        <SizeMe>
+                            {({size}) =>
+                                <GeneTransformerGraph
+                                    nodes={ledgerTo("nodes")(this.props.transactionHistory)}
+                                    links={ledgerTo("nodes")(this.props.transactionHistory)}
+                                    width={size.width}
+                                    height={size.height}
+                                />}
+                        </SizeMe>
+                    </Card>
+                    : <React.Fragment/>}
+            </div>
+        )
+    }
+}
+
+class GraphTest extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleResize(e) {
+        let elem = ReactDOM.findDOMNode(this);
+        let width = elem.offsetWidth;
+
+        this.setState({
+            parentWidth: width
+        });
+    }
+
+    componentDidMount() {
+        if(this.props.width === '100%') {
+            window.addEventListener('resize', this.handleResize);
+        }
+        this.handleResize();
+    }
+
+    componentWillUnmount() {
+        if(this.props.width === '100%') {
+            window.removeEventListener('resize', this.handleResize);
+        }
+    }
+
+    render() {
+        let { width, height, margin, xScale, yScale, xAxis, ...props } = this.props;
+
+        // Determine the right graph width to use if it's set to be responsive
+        if(width === '100%') {
+            width = this.state.parentWidth || 400;
+        }
+
+        // Set scale ranges
+        xScale && xScale.range([0, width - (margin.left + margin.right)]);
+        yScale && yScale.range([height - (margin.top + margin.bottom), 0]);
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+// container component for dimensions
+// https://stackoverflow.com/a/57272554
+//https://github.com/codesuki/react-d3-components/issues/9
+// const GeneTransformerHistory = props => {
+//     return (
+//         <div ref={targetRef}>
+//             <GeneTransformerGraph nodes={props.nodes} links={props.links}
+//                                   width={dimensions.width} height={dimensions.height}/>
+//         </div>
+//     );
+// };
+
+const GeneTransformerGraph = (props) => {
+    useEffect(() => {
+        drawGraph(props);
+    }, [props.nodes, props.links]);
+    return <div className={"viz"}/>;
+};
