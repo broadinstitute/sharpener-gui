1dab96bf1a6d8e86133e0ccfa311f2e48fcbba5c
Kenneth Bruskiewicz <kenneth@starinformatics.com>
2019-08-27 16:58:22 -0700

    PATCH: PATCH: Adding the helpers into the quality-of-life revisions

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,24 @@
+import React from "react";
+
 export const tap = (data) => {
     console.log(data);
     return data;
 };
+
+export const properCase = (string) => {
+    const fillwords = [
+        "a", "with", "for"
+    ];
+    string = underscoreToSpaces(string);
+    return string.split(' ').map(function(word){
+        return !fillwords.includes(word) ? word.charAt(0).toUpperCase() + word.substr(1).toLowerCase() : word;
+    }).join(' ');
+};
+
+export const pluralize = (length, word) => {
+    return length + ' ' + word + (length > 1 ? "s" : length <= 0 ? "s" : '')
+};
+
+export const underscoreToSpaces = (string) => {
+    return string.split('_').join(' ');
+}
