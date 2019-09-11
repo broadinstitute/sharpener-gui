912ee9dc309c552c51c3d371ce28edfaace4188b
Kenneth Bruskiewicz <kenneth@starinformatics.com>
2019-09-10 19:43:06 -0700

    PATCH: make modal navigation appear earlier

--- src/components/GeneFeed/GeneFeed.js
+++ src/components/GeneFeed/GeneFeed.js
@@ -95,7 +95,7 @@ export default class GeneTabs extends React.Component {
         });
         return (
             <div className={"col-sm-12"}>
-                <Tabs customStyle={customStyle} showModalButton={2}>
+                <Tabs customStyle={customStyle} showModalButton={3}>
                     <TabList>
                         {tabsTemplate}
                     </TabList>
